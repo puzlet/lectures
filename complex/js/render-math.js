3fb6314153830ebecddbe1c3b8ed83e51aff1ede
Martin Clark <martinclark@mac.com>
2016-05-18 10:55:36 +1200

    window.console

--- complex/js/render-math.js
+++ complex/js/render-math.js
@@ -1,6 +1,8 @@
 /* From https://github.com/Khan/KaTeX/tree/master/contrib/auto-render */
 
 // Default page rendering
+if (!window.console) window.console = {};
+if (!window.console.log) window.console.log = function() {};
 console.log("Render KaTeX");
 renderMathInElement(
   document.getElementById("container"), {
